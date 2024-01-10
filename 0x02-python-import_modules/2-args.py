a98c0c14d51fa32ed70b1e0e27943868472e24f3
Kelvin <qelvynn@gmail.com>
2024-01-10 15:52:25 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     size = len(arg) - 1
 
     if size > 1:
-        print("{} arguments:".format(size))
+        print("{} argument(s):".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arg[i]))
 
-    elif size == 0:
-        print("{} arguments.".format(size))
+    elif size == 1:
+        print("{} argument(s):".format(size)) 
+        print("{}: {}".format(size, arg[1]))
 
     else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+        print("{} argument(s).".format(size)
