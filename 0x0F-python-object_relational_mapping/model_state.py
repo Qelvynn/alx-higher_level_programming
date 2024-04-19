622040dd6ae8eeddadf475146a86e6d0f6c21c9f
Kelvin <qelvynn@gmail.com>
2024-04-19 13:09:50 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
-"""Module that lists all cities from a specified state in the hbtn_0e_0_usa database."""
+"""Module that defines the State class\
+        representing a state in a MySQL database."""
 
-import sys
-import MySQLdb
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-def main():
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state for a MySQL database.
+
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
     """
-    Main function that fetches and prints cities from the database
-    that are in a specified state provided as a command-line argument.
-    """
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        return
-
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    try:
-        db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database)
-        with db.cursor() as cursor:
-            query = """
-            SELECT c.name
-            FROM cities AS c
-            JOIN states AS s ON c.state_id = s.id
-            WHERE s.name = %s
-            ORDER BY c.id
-            """
-            cursor.execute(query, (state_name,))
-            cities = [city[0] for city in cursor.fetchall()]
-            print(", ".join(cities))
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-    finally:
-        if db:
-            db.close()
-
-if __name__ == "__main__":
-    main()
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
